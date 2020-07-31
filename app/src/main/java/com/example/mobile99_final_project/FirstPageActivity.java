e2c14ac755b89f2d99b5c0f308a1d258077eea0b
RezaTaheri <h.rezataheri@gmail.com>
2020-07-31 14:31:01 +0430

    files that later need to be developed

---
+++ app/src/main/java/com/example/mobile99_final_project/FirstPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile99_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
