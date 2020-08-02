1e2ebdc51c47e9bf62cc4b588b8e924c7ad0fd63
RezaTaheri <h.rezataheri@gmail.com>
2020-08-02 07:16:48 +0430

    Added message codes required in the future

--- app/src/main/java/com/example/mobile99_final_project/Enums/HandlerMassages.java
+++ app/src/main/java/com/example/mobile99_final_project/Enums/HandlerMassages.java
@@ -4,5 +4,19 @@ public class HandlerMassages {
     public static final int LOGIN_MESSAGE = 0;
     public static final int SUCCESSFUL_LOGIN_MESSAGE = 2;
     public static final int TOKEN_CHECK_SUCCESS = 4;
+    public static final int SIGNUP_START = 6;
+    public static final int SIGNUP_SUCCESS = 7;
+    public static final int SIGNUP_FAIL = 8;
+    public static final int GET_COORDS = 9;
+    public static final int GET_CITY = 10;
+    public static final int SET_CITY = 11;
+    public static final int GET_CATS = 12;
+    public static final int SHOW_FETCHED_CATS = 13;
+    public static final int SHOW_SUB_CATS = 14;
+    public static final int FETCH_ADS = 15;
+    public static final int SHOW_ADS = 16;
+    public static final int SHOW_AD_DETAILS = 17;
+    public static final int CATS_FETCHED = 18;
+    public static final int FILTER_BY_CAT = 19;
 
 }
