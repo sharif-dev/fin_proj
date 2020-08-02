7bec044d700ad3b98a7732357318bd48b6b5df0b
RezaTaheri <h.rezataheri@gmail.com>
2020-08-02 07:04:36 +0430

    Enum for action handle messages

---
+++ app/src/main/java/com/example/mobile99_final_project/Enums/HandlerMassages.java
@@ -0,0 +1,8 @@
+package com.example.mobile99_final_project.Enums;
+
+public class HandlerMassages {
+    public static final int LOGIN_MESSAGE = 0;
+    public static final int SUCCESSFUL_LOGIN_MESSAGE = 2;
+    public static final int TOKEN_CHECK_SUCCESS = 4;
+
+}
