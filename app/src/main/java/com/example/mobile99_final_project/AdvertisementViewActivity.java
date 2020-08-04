ffe5633e56aa3d1c9311788af12f8989318e709b
mohammad saleh <salehsagharchi@gmail.com>
2020-08-04 20:29:34 +0430

    create AdvertisementViewActivity create activity_advertisement_view.xml

--- app/src/main/java/com/example/mobile99_final_project/AdvertisementViewActivity.java
+++ app/src/main/java/com/example/mobile99_final_project/AdvertisementViewActivity.java
@@ -3,12 +3,47 @@ package com.example.mobile99_final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mobile99_final_project.DataModels.AdData;
+import com.example.mobile99_final_project.DataModels.CategoryData;
+import com.example.mobile99_final_project.DataModels.CategoryList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AdvertisementViewActivity extends AppCompatActivity {
+
+    TextView titleTextView;
+    TextView ownerTextView;
+    TextView catTextView;
+    TextView descriptionTextView;
+
+    ArrayList<CategoryData> categoryDataList;
+    HashMap<Integer, String> categoryHashMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advertisement_view);
+
+        titleTextView = findViewById(R.id.ad_details_title_textView);
+        ownerTextView = findViewById(R.id.ad_details_owner_textView);
+        catTextView = findViewById(R.id.ad_details_cat_textView);
+        descriptionTextView = findViewById(R.id.ad_details_description);
+
+        AdData adData = (AdData) getIntent().getSerializableExtra("AdData");
+        categoryDataList = (ArrayList<CategoryData>) getIntent().getSerializableExtra("categoryDataList");
+        CategoryList cl = new CategoryList(categoryDataList);
+        categoryHashMap = cl.getCategoryHashMap();
+
+        if (adData != null){
+            titleTextView.setText(adData.title);
+            ownerTextView.setText(adData.owner);
+            System.out.println(adData.category);
+            catTextView.setText(categoryHashMap.get(adData.category));
+            descriptionTextView.setText(adData.description);
+        }
     }
 }
